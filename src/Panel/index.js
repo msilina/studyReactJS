5afbf0c8b1254237287f64ab41377ed4005bdcdd
Maryna Silina <msilina@gomel.iba.by>
2021-01-06 16:31:32 +0300

    task2

--- src/Panel/index.js
+++ src/Panel/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Panel.css';
 
-const Panel = (props) => {
-    return (
-        <div className="panel-list">
-            <div className="panel">
-                <h2>{ props.caption }</h2>
-                <hr/>
-                <p>{ props.text }</p>
+class Panel extends Component {
+
+    state = {
+        checked: false
+    }
+
+    changeColorHandler = () => {
+        this.setState({
+            checked: !this.state.checked
+        })
+    }
+
+    render() {
+        let panelColor = this.state.checked ? "panel-checked" : "panel-default";
+
+        return (
+            <div className="panel-list">
+                <div className={ panelColor } >
+                        <table className="panel-header">
+                            <tr>
+                                <td>
+                                    <h2>{ this.props.caption }</h2>
+                                </td>
+                                <td className="align-right">
+                                    <input onClick={ this.changeColorHandler } type="checkbox" id="check"/>
+                                </td>
+                            </tr>
+                        </table>
+                        <hr/>
+                        <p>{ this.props.text }</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Panel;
